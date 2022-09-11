c41e083172ddb68035477ab7583854dcacdb2c96
Sergey Kuznetsov <nelsonff918@gmail.com>
2022-09-11 16:36:52 +0300

    Update ConvertRequest.php

--- ex2/advance/api/models/ConvertRequest.php
+++ ex2/advance/api/models/ConvertRequest.php
@@ -48,62 +48,62 @@ class ConvertRequest extends Model
 	public function DataResponse() 
 	{
 		
-        $json_error = [
+        	$json_error = [
 
-            'status' => 'error',
-            'code' => 400,
-            'message' => '',
+            	'status' => 'error',
+            	'code' => 400,
+            	'message' => '',
 
-        ];
+        	];
 
-        if (!$this->currency_from or !$this->currency_to or !$this->value) {
-        	$json_error['message'] = 'Incorrect parameters';
-        	return $json_error;
-        }
+        	if (!$this->currency_from or !$this->currency_to or !$this->value) {
+        		$json_error['message'] = 'Incorrect parameters';
+        		return $json_error;
+        	}
 
-        if((!array_key_exists($this->currency_from,$this->ticker) and $this->currency_from != 'BTC') or (!array_key_exists($this->currency_to,$this->ticker) and $this->currency_to != 'BTC')){
+        	if((!array_key_exists($this->currency_from,$this->ticker) and $this->currency_from != 'BTC') or (!array_key_exists($this->currency_to,$this->ticker) and $this->currency_to != 'BTC')){
 			$json_error['message'] = 'Undefined currency';
 			return $json_error;
-        }
+        	}
 
-        if ($this->currency_from == $this->currency_to) {
-        	$json_error['message'] = 'Сurrencies should be different';
-        	return $json_error;
-        }
+        	if ($this->currency_from == $this->currency_to) {
+        		$json_error['message'] = 'Сurrencies should be different';
+        		return $json_error;
+        	}
 
-        if ($this->currency_from != 'BTC' and $this->currency_to != 'BTC') {
-        	$json_error['message'] = 'Unsupported exchange';
-        	return $json_error;
-        }
+        	if ($this->currency_from != 'BTC' and $this->currency_to != 'BTC') {
+        		$json_error['message'] = 'Unsupported exchange';
+        		return $json_error;
+        	}
 
-        $json_convert = [
-            'status' => 'success',
-            'code' => 200,
-            'data' => [
-                'convert_from' => $this->currency_from,
-                'convert_to' => $this->currency_to,
-                'value' => $this->value,
-                'converted_value' => round($this->calcConvertSum(), 10),
-                'rate' => round($this->calcConvertSum()/$this->value, 10),
-            ],
-        ];
+        	$json_convert = [
+            		'status' => 'success',
+            		'code' => 200,
+            		'data' => [
+                		'convert_from' => $this->currency_from,
+                		'convert_to' => $this->currency_to,
+                		'value' => $this->value,
+                		'converted_value' => round($this->calcConvertSum(), 10),
+                		'rate' => round($this->calcConvertSum()/$this->value, 10),
+            		],
+        	];
 
-        if ($this->value < 0.01) {
-        	$json_error['message'] = 'Value should be more than 0.01';
-        	$json_error['code'] = 403;
-        	return $json_error;
-        }else{
-        	return $json_convert;
-        }
+        	if ($this->value < 0.01) {
+        		$json_error['message'] = 'Value should be more than 0.01';
+        		$json_error['code'] = 403;
+			return $json_error;
+        	}else{
+        		return $json_convert;
+        	}
 	}
 
 	public function rules()
-    {
-        return [
-            // currency_from, currency_to, and value are required
-            [['currency_from', 'currency_to', 'value'], 'required'],
-        ];
-    }
+    	{
+        	return [
+            		// currency_from, currency_to, and value are required
+            		[['currency_from', 'currency_to', 'value'], 'required'],
+        	];
+    	}
 
 }
 
